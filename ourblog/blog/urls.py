e92c608e23f5df6a1023b07e475af59da4a6e03c
zayyaski <zayyadidris294@gmail.com>
2021-07-10 18:29:22 +0100

    zayyad's commit

--- ourblog/blog/urls.py
+++ ourblog/blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
+    path('', views.index, name='blog'),
 ]
--- ourblog/blog/views.py
+++ ourblog/blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Article
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 def index(request):
+    context = {
+        'posts': Article.objects.all()
+    }
     return HttpResponse("Hello, world. You're at the pblog index page.")
