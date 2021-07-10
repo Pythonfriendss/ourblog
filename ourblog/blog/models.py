ee647eaabc134b3279672a0571c81e992f0feda6
zayyaski <zayyadidris294@gmail.com>
2021-07-10 20:47:45 +0100

    zayyad's second commit

--- ourblog/blog/models.py
+++ ourblog/blog/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+from django.contrib.auth.models import User
+from PIL import Image
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 class Article(models.Model):
     title = models.CharField(max_length=150)
+    author = models.ForeignKey(User, models.PROTECT)
     article_text = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
 
 
 class Comment(models.Model):
-    comment = models.ForeignKey(Article, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=200)
+    post = models.ForeignKey(Article,on_delete=models.CASCADE,related_name='comments')
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"comment {self.body} by {self.name}"
--- ourblog/blog/views.py
+++ ourblog/blog/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Article, Comment
 from django.urls import reverse
-from django.views import generic
+from django.views.generic import ListView, DetailView, CreateView
 
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
+    model = Article
+    context_object_name = 'article'
     template_name = 'blog/index.html'
 
     def get_queryset(self):
         return Article.objects.order_by('-pub_date')[:10]
     
+class ArticleDetailView(DetailView):
+    model = Article
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ['title', 'content']
