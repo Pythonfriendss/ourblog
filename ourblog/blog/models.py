d8ad4ba6c8c1c6268ca41d2589e1f418fca0630c
Pythonfriendss <816@wp.pl>
2021-07-10 17:56:50 +0200

    Barebones_commit

--- ourblog/blog/models.py
+++ ourblog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_text = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
+
+
+class Comment(models.Model):
+    comment = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
