5633204695d695c3724b9c23b5bd82831d22adc2
zayyaski <zayyadidris294@gmail.com>
2021-07-11 21:01:37 +0100

    master commit

--- ourblog/blog/admin.py
+++ ourblog/blog/admin.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from .models import Article, Comment
 
 
-<<<<<<< HEAD
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Comment)
-=======
 from .models import Article, Comment
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -17,4 +15,3 @@ class CommentAdmin(admin.ModelAdmin):
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment, CommentAdmin)
->>>>>>> 7531963e5667724879b77948df21733b641b9b63
